97e139688d64ad0d3b274f635df05eacea2f03b6
novasenco <novasenco@protonmail.ch>
2021-08-22 22:02:42 -0400

    Make all scripts POSIX compliance
    FOR SPEED ♥ because now dash can run these

--- scripts/toggletouchpad_libinput.sh
+++ scripts/toggletouchpad_libinput.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # TODO: find touchpad name on new system
-id="$(xinput --list --id-only 'ELAN1200:00 04F3:30BA Touchpad')"
-xinput list-props $id | egrep 'Device Enabled\s+\S+\s+1' >/dev/null 2>&1 &&\
-    xinput disable $id ||\
-    xinput enable $id
+id="$(xinput --list --id-only 'ELAN1200:00 04F3:30BA Touchpad')";
+if xinput list-props $id | egrep 'Device Enabled\s+\S+\s+1' >/dev/null 2>&1;
+then
+  xinput disable "$id";
+else
+  xinput enable "$id";
+fi;
